08fc6bbab76cb40af0c6d6c83422af30c17bb659
André Correia <andrecorreis@gmail.com>
2026-01-08 22:23:30 +0000

    refactor(UtilizadoresController): replace password encryption with BCrypt hashing and update action name refactor(UtilizadorLoginDTO): rename class and update password property to plain text

--- backend/API/ProjetoAdministracaoEscola/Controllers/UtilizadoresController.cs
+++ backend/API/ProjetoAdministracaoEscola/Controllers/UtilizadoresController.cs
@@ -80,12 +80,12 @@ namespace ProjetoAdministracaoEscola.Controllers
         public async Task<ActionResult<Utilizador>> PostUtilizador(Utilizador utilizador)
         {
 
-            utilizador.PasswordHash = EncryptString(utilizador.PasswordHash);
+            utilizador.PasswordHash = BCrypt.Net.BCrypt.HashPassword(utilizador.PasswordHash);
 
             _context.Utilizadores.Add(utilizador);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUtilizadore", new { id = utilizador.IdUtilizador }, utilizador);
+            return CreatedAtAction("GetUtilizadores", new { id = utilizador.IdUtilizador }, utilizador);
         }
 
         // DELETE: api/Utilizadores/5
@@ -107,153 +107,6 @@ namespace ProjetoAdministracaoEscola.Controllers
         private bool UtilizadorExists(int id)
         {
             return _context.Utilizadores.Any(e => e.IdUtilizador == id);
-        }
-
-        public static string EncryptString(string Message)
-
-        {
-
-            string Passphrase = "oMeuChaPeuTemTrezEntosE?SesSentAeNoveBi?cosEquaTroCa!nt#os";
-
-            byte[] Results;
-
-            System.Text.UTF8Encoding UTF8 = new System.Text.UTF8Encoding();
-
-            // Step 1. We hash the passphrase using MD5
-
-            // We use the MD5 hash generator as the result is a 128 bit byte array
-
-            // which is a valid length for the TripleDES encoder we use below
-
-            MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
-
-            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(Passphrase));
-
-            // Step 2. Create a new TripleDESCryptoServiceProvider object
-
-            TripleDESCryptoServiceProvider TDESAlgorithm = new TripleDESCryptoServiceProvider();
-
-            // Step 3. Setup the encoder
-
-            TDESAlgorithm.Key = TDESKey;
-
-            TDESAlgorithm.Mode = CipherMode.ECB;
-
-            TDESAlgorithm.Padding = PaddingMode.PKCS7;
-
-            // Step 4. Convert the input string to a byte[]
-
-            byte[] DataToEncrypt = UTF8.GetBytes(Message);
-
-            // Step 5. Attempt to encrypt the string
-
-            try
-
-            {
-
-                ICryptoTransform Encryptor = TDESAlgorithm.CreateEncryptor();
-
-                Results = Encryptor.TransformFinalBlock(DataToEncrypt, 0, DataToEncrypt.Length);
-
-            }
-
-            finally
-
-            {
-
-                // Clear the TripleDes and Hashprovider services of any sensitive information
-
-                TDESAlgorithm.Clear();
-
-                HashProvider.Clear();
-
-            }
-
-            // Step 6. Return the encrypted string as a base64 encoded string
-
-            string enc = Convert.ToBase64String(Results);
-
-            enc = enc.Replace("+", "KKK");
-
-            enc = enc.Replace("/", "JJJ");
-
-            enc = enc.Replace("\\", "III");
-
-            return enc;
-
-        }
-
-        public static string DecryptString(string Message)
-
-        {
-
-            string Passphrase = "oMeuChaPeuTemTrezEntosE?SesSentAeNoveBi?cosEquaTroCa!nt#os";
-
-            byte[] Results;
-
-            System.Text.UTF8Encoding UTF8 = new System.Text.UTF8Encoding();
-
-            // Step 1. We hash the passphrase using MD5
-
-            // We use the MD5 hash generator as the result is a 128 bit byte array
-
-            // which is a valid length for the TripleDES encoder we use below
-
-            MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
-
-            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(Passphrase));
-
-            // Step 2. Create a new TripleDESCryptoServiceProvider object
-
-            TripleDESCryptoServiceProvider TDESAlgorithm = new TripleDESCryptoServiceProvider();
-
-            // Step 3. Setup the decoder
-
-            TDESAlgorithm.Key = TDESKey;
-
-            TDESAlgorithm.Mode = CipherMode.ECB;
-
-            TDESAlgorithm.Padding = PaddingMode.PKCS7;
-
-            // Step 4. Convert the input string to a byte[]
-
-            Message = Message.Replace("KKK", "+");
-
-            Message = Message.Replace("JJJ", "/");
-
-            Message = Message.Replace("III", "\\");
-
-
-            byte[] DataToDecrypt = Convert.FromBase64String(Message);
-
-            // Step 5. Attempt to decrypt the string
-
-            try
-
-            {
-
-                ICryptoTransform Decryptor = TDESAlgorithm.CreateDecryptor();
-
-                Results = Decryptor.TransformFinalBlock(DataToDecrypt, 0, DataToDecrypt.Length);
-
-            }
-
-            finally
-
-            {
-
-                // Clear the TripleDes and Hashprovider services of any sensitive information
-
-                TDESAlgorithm.Clear();
-
-                HashProvider.Clear();
-
-            }
-
-            // Step 6. Return the decrypted string in UTF8 format
-
-            return UTF8.GetString(Results);
-
         }
 
     }
--- backend/API/ProjetoAdministracaoEscola/Models/UtilizadorLoginDTO.cs
+++ backend/API/ProjetoAdministracaoEscola/Models/UtilizadorLoginDTO.cs
@@ -3,25 +3,9 @@ using System.Collections.Generic;
 
 namespace ProjetoAdministracaoEscola.Models;
 
-public partial class Utilizador
+public partial class UtilizadorLoginDTO
 {
-    public int IdUtilizador { get; set; }
-
     public string Email { get; set; } = null!;
 
-    public string PasswordHash { get; set; } = null!;
-
-    public string? IdGoogle { get; set; }
-
-    public string? IdFacebook { get; set; }
-
-    public int IdTipoUtilizador { get; set; }
-
-    public bool? StatusAtivacao { get; set; }
-
-    public virtual ICollection<Formadore> Formadores { get; set; } = new List<Formadore>();
-
-    public virtual ICollection<Formando> Formandos { get; set; } = new List<Formando>();
-
-    public virtual TipoUtilizadore IdTipoUtilizadorNavigation { get; set; } = null!;
+    public string Password { get; set; } = null!;
 }
