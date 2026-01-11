9d2dc19162378789564f29f3052fb962217bc7a2
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-11 16:48:41 +0000

    feat(LandingPage): basic layout structure

--- frontend/HAWKTUAH/src/pages/LandingPage.tsx
+++ frontend/HAWKTUAH/src/pages/LandingPage.tsx
@@ -1,8 +1,30 @@
+import { Link } from "react-router-dom"
 
 export default function LandingPage() {
   return (
     <>
-      teste
+      <div className="container">
+        <div className="title-dashboard d-flex justify-content-between w-100">
+          <div className="title-dashboard-left ">
+            <h2>
+              Dasboard
+              <br />
+            </h2>
+            <span className="text-muted">
+              Informação Rápida sobre o Sistema
+            </span>
+          </div>
+          <div className="title-dashboard-right justify-content-end">
+            <Link to="/assistenteAI" className="btn rounded">Assistente AI</Link>
+            <Link to="/horarios" className="btn rounded">Ver Horários</Link>
+          </div>
+        </div>
+        <div className="mt-5">
+          aqui vai ser vários cards feito em componente
+        </div>
+        <div className="mt-5">2 CARDS   </div>
+        <div className="mt-5">Cursos por área</div>
+      </div>
     </>
   );
 }
