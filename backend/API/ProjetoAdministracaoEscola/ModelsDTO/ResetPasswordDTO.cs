397131b43b321ba424f3928a3e1cbfe8e6dcbdea
André Correia <andrecorreis@gmail.com>
2026-01-13 18:59:42 +0000

    feat(ResetPassword): implement reset password functionality with form validation

---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/ResetPasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO
+{
+    public class ResetPasswordDTO
+    {
+        public string Token { get; set; } = null!;
+
+        public string NewPassword { get; set; } = null!;
+    }
+}
