f36a742da31e3f72299507e6e64dca14d2ffd800
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-14 21:33:34 +0000

    feat(formandoService): add service for fetching 'Formando' data from API

---
+++ frontend/HAWKTUAH/src/services/formandoService.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { API_BASE_URL } from "../config.constants";
+
+export interface Formando {
+  id: number;
+  nome: string;
+  email: string;
+}
+
+export async function getFormandos() {
+  const res = await axios.get(`${API_BASE_URL}/formandos`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+
+  return res.data as Formando[];
+}
