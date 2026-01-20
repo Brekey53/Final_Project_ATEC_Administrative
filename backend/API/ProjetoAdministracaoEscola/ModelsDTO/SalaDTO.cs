18a824c2a9bb8d5cd62c5c42746a2957f933a454
André Correia <andrecorreis@gmail.com>
2026-01-20 22:56:29 +0000

    Add SalasDTO and ModuloUpdateDTO

---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/SalaDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAdministracaoEscola.ModelsDTO
+{
+    public class SalaDTO
+    {
+        public int IdSala { get; set; } // Opcional no POST, obrigatório no PUT
+
+        [Required(ErrorMessage = "A descrição da sala é obrigatória.")]
+        [StringLength(100, ErrorMessage = "A descrição não pode exceder 100 caracteres.")]
+        public string Descricao { get; set; }
+
+        [Range(1, 200, ErrorMessage = "A lotação deve estar entre 1 e 200 alunos.")]
+        public int NumMaxAlunos { get; set; }
+    }
+}
