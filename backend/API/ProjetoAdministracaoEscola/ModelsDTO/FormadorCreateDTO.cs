a286b240b6853f6c8868d2e03eb29c193b802438
André Correia <andrecorreis@gmail.com>
2026-01-22 23:47:54 +0000

    Create a FormadorCreateDTO to recieve the user input

---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/FormadorCreateDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO
+{
+    public class FormadorCreateDTO
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string Nome { get; set; }
+        public string Nif { get; set; }
+        public string Telefone { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public string Morada { get; set; }
+
+        // IFormFile permite receber os ficheiros do FormData
+        public IFormFile? Fotografia { get; set; }
+        public IFormFile? Documento { get; set; }
+    }
+}
