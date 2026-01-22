db3b38cdce426c5f52ba6da5d6a68861f2ba0d9f
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-22 03:28:23 +0000

    feat(footer): tentativa de footer até perceber que o LUCIDE JÁ NÃO TEM ICONES PARA INSTA E LINKEDIN PORQUE?????????

--- frontend/HAWKTUAH/src/components/Footer.tsx
+++ frontend/HAWKTUAH/src/components/Footer.tsx
@@ -1,7 +1,63 @@
-import '../css/footer.css'
+import "../css/footer.css";
+import { Mail, Phone, Instagram, Linkedin, Github } from "lucide-react";
 
 export default function Footer() {
   return (
-    <div className='text-center footer mt-5'>©Footer</div>
-  )
+    <footer className="footer mt-5 py-5 bg-light">
+      <div className="container">
+        <div className="row align-items-center">
+
+          <div className="col-md-4 text-center text-md-start mb-3 mb-md-0">
+            <h5 className="fw-bold mb-2">HawkPortal</h5>
+            <small>
+              Sistema de Gerir Notas
+            </small>
+          </div>
+
+          {/* Contactos */}
+          <div className="col-md-4 text-center mb-3 mb-md-0">
+            <p className="mb-1">
+              <Mail size={16} className="me-2" />
+              hawkportal@gmail.com
+            </p>
+            <p className="mb-0">
+              <Phone size={16} className="me-2" />
+              +351 912 345 678
+            </p>
+          </div>
+
+          <div className="col-md-4 text-center text-md-end">
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-dark me-3"
+            >
+              <Instagram />
+            </a>
+
+            <a
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-dark me-3"
+            >
+              <Linkedin />
+            </a>
+
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-dark"
+            >
+              <Github />
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+
+  );
 }
