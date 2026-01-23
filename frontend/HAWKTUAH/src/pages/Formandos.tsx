01de5474ea69da9faaea3c8dda4b65a8c961cacd
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-23 19:59:31 +0000

    fix(bird formados): deleted unecessary svg

--- frontend/HAWKTUAH/src/pages/Formandos.tsx
+++ frontend/HAWKTUAH/src/pages/Formandos.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import AnimatedLogo from "../components/AnimatedLogo";
+
 
 export default function Formandos() {
   return (
     <section className="hero">
-      <AnimatedLogo />
       <div>Formandos</div>
     </section>
   );
