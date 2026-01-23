3099d388182b67440fa0f27a8daf94e0e61ce986
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-23 21:21:11 +0000

    feat(new pages): new pages for dashboard depending on users role

---
+++ frontend/HAWKTUAH/src/components/dashboard/FormadorDashboard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {useState} from "react"
+
+import Cursos from "../../pages/course/Cursos"
+import Horarios from "../../pages/schedule/Schedules"
+
+export default function FormadorDashboard() {
+  type Tab = "cursos" | "horarios";
+  const [activeTab, setActiveTab] = useState<Tab>("cursos");
+  return (
+    <div className="container my-5">
+      <h2>Dashboard</h2>
+      <div className="dashboard-pesquisa shadow p-4">
+        <form className="d-flex gap-2">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Pesquisar curso"
+          />
+          <button type="submit" className="btn btn-primary">
+            Pesquisar
+          </button>
+        </form>
+      </div>
+      <div>
+        <div className="d-flex justify-content-around mt-5 tab-div">
+          <button
+            className={activeTab === "cursos" ? "tab-active" : ""}
+            onClick={() => setActiveTab("cursos")}
+          >
+            Cursos
+          </button>
+
+          <button
+            className={activeTab === "horarios" ? "tab-active" : ""}
+            onClick={() => setActiveTab("horarios")}
+          >
+            Horários
+          </button>
+        </div>
+        <div className="mt-4">
+          {activeTab === "cursos" && <Cursos />}
+          {activeTab === "horarios" && <Horarios />}
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/HAWKTUAH/src/pages/LandingPage.tsx
+++ frontend/HAWKTUAH/src/pages/LandingPage.tsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import DashboardIntro from "../components/DashboardIntro";
-import DashboardContent from "../components/DashboardContent";
+import AdminDashboard from "../components/dashboard/AdminDashboard";
+import FormadorDashboard from "../components/dashboard/FormadorDashboard";
+import FormandoDashboard from "../components/dashboard/FormandoDashboard";
+import GeralDashboard from "../components/dashboard/GeralDashboard";
+import { authService } from "../auth/AuthService";
+import { mapUserRole } from "../auth/MapUserRole";
 
 export default function Dashboard() {
   const [showIntro, setShowIntro] = useState(() => {
     return !sessionStorage.getItem("dashboardIntroShown");
   });
+
+  const user = authService.decodeToken();
+  if (!user) 
+    return null;
+
+  const role = mapUserRole(Number(user.tipoUtilizador));
 
   if (showIntro) {
     return (
@@ -18,5 +29,18 @@ export default function Dashboard() {
     );
   }
 
-  return <DashboardContent />;
+  switch (role) {
+    case "ADMIN":
+      return <AdminDashboard />;
+
+    case "FORMADOR":
+      return <FormadorDashboard />;
+
+    case "FORMANDO":
+      return <FormandoDashboard />;
+
+    case "GERAL":
+    default:
+      return <GeralDashboard />;
+  }
 }
