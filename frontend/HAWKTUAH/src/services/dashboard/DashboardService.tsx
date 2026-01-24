fef727e8d094032e53a88b12c030c65b3bffd4ae
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-24 19:55:33 +0000

    feat(DashboardService): added api calls to retrieve information for the admin dashboard

--- frontend/HAWKTUAH/src/services/dashboard/DashboardService.tsx
+++ frontend/HAWKTUAH/src/services/dashboard/DashboardService.tsx
@@ -9,8 +9,45 @@ export interface DashboardStats {
   salas: number;
   modulos: number;
 }
+export interface CursosADecorrer {
+  idTurma: number;
+  nomeTurma: string;
+  nomeCurso: string;
+  dataInicio: string;
+  dataFim: string;
+}
+
+export interface TurmaAIniciar {
+  idTurma: number;
+  nomeTurma: string;
+  nomeCurso: string;
+  dataInicio: string;
+  dataFim: string;
+}
+
+export interface CursosPorArea {
+  idArea: number;
+  nomeArea: string;
+  totalCursos: number;
+}
 
 export async function getDashboardStats(): Promise<DashboardStats> {
   const res = await axios.get(`${API_BASE_URL}/dashboard/stats`);
+  return res.data;
+}
+
+
+export async function getCursosADecorrer() {
+  const res = await axios.get(`${API_BASE_URL}/dashboard/turmasDecorrer`);
+  return res.data;
+}
+
+export async function getTurmasAIniciar() {
+  const res = await axios.get("/dashboard/turmasAIniciar");
+  return res.data as TurmaAIniciar[];
+}
+
+export async function getCursosPorArea(): Promise<CursosPorArea[]> {
+  const res = await axios.get(`${API_BASE_URL}/dashboard/cursosPorArea`);
   return res.data;
 }
