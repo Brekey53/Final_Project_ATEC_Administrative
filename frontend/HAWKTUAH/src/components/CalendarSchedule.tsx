2208d40533124f39167a20a7dda43511453827c1
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-24 18:56:40 +0000

    feat(CalendarSchedule): Component calendar from fullCalendar to display calendar on dashboard

---
+++ frontend/HAWKTUAH/src/components/CalendarSchedule.tsx
@@ -0,0 +1,19 @@
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import ptLocale from "@fullcalendar/core/locales/pt";
+
+export default function CalendarSchedule({ events }: { events: any[] }) {
+  return (
+    <FullCalendar
+      plugins={[timeGridPlugin]}
+      initialView="timeGridWeek"
+      locale={ptLocale}
+      events={events}
+      allDaySlot={false}
+      slotMinTime="08:00:00"
+      slotMaxTime="22:00:00"
+      height="auto"
+      nowIndicator
+    />
+  );
+}
