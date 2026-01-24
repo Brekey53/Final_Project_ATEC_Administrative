cce6e9dcd288d40c8613b61de596372ad457f216
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-24 19:57:15 +0000

    feat(CursosPorAreaDTO): added DTO for retrieving information on Cursos por Área
    feat(TurmaDecorrer): DTO to retrieve information to display about classes that are ongoing now and future classes within the next 60 days

---
+++ backend/API/ProjetoAdministracaoEscola/Models/CursosPorAreaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoAdministracaoEscola.Models
+{
+    public class CursosPorAreaDTO
+    {
+        public int IdArea { get; set; }
+        public string NomeArea { get; set; } = string.Empty;
+        public int TotalCursos { get; set; }
+    }
+}
