820b6a76bdf9abbc05787946b1f8b4a9b0e4ed55
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-24 04:31:35 +0000

    fix(UtilizadorUpdateDTO): added idTipoUtilizador

--- backend/API/ProjetoAdministracaoEscola/ModelsDTO/UtilizadorUpdateDTO.cs
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/UtilizadorUpdateDTO.cs
@@ -7,6 +7,8 @@
         public DateOnly DataNascimento { get; set; }
         public string? Morada { get; set; }
         public string? Telefone { get; set; }
+
+        public int IdTipoUtilizador { get; set; }
         public string? Sexo { get; set; }
         public string? Email { get; set; }
     }
