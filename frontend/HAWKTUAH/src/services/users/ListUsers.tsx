e5b6a6ca14b1aa4b12a139d2bfef5784d1ceafd9
André Correia <andrecorreis@gmail.com>
2026-01-24 13:59:25 +0000

    minor change in ListUsers (is to remove?)

--- frontend/HAWKTUAH/src/services/users/ListUsers.tsx
+++ frontend/HAWKTUAH/src/services/users/ListUsers.tsx
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "axios";
 import { API_BASE_URL } from "../../config.constants";
 
 export interface Utilizador {
@@ -9,7 +9,7 @@ export interface Utilizador {
   tipoUtilizador: string;
 }
 
-export async function getUtilizadores() {
+export async function getUtilizadores(): Promise<Utilizador[]> {
   const res = await axios.get(`${API_BASE_URL}/utilizadores`);
 
   return res.data;
