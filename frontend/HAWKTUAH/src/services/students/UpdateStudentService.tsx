eaa9b056b7e036660a1dcb2959067ab976e5acf7
André Correia <andrecorreis@gmail.com>
2026-01-25 22:58:09 +0000

    Delete obsolete Services

--- frontend/HAWKTUAH/src/services/students/UpdateStudentService.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { API_BASE_URL } from "../../config.constants";
-
-export async function updateFormando(id: string, data: FormData) {
-  // O Axios configura o Content-Type: multipart/form-data automaticamente ao receber FormData
-  const res = await axios.put(`${API_BASE_URL}/formandos/${id}`, data);
-  return res.data;
-}
