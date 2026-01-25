c14970268c1b203268b4cd2c9b34db13db39c9d2
André Correia <andrecorreis@gmail.com>
2026-01-25 22:58:43 +0000

    Group all of FormandoServices in one file

--- frontend/HAWKTUAH/src/services/students/formandoService.tsx
+++ frontend/HAWKTUAH/src/services/students/formandoService.tsx
@@ -5,33 +5,64 @@ import { API_BASE_URL } from "../../config.constants";
 export interface Formando {
   idFormando: number;
   nome: string;
-  email: string;
   nif: string;
+  dataNascimento: string;
+  morada: string;
+  telefone?: string;
+  sexo: string;
+  email: string;
   turma: string;
-  status?: boolean;
-  phone?: string;
+  idEscolaridade: string;
+  fotografia: File | null;
+  anexoFicheiro: File | null;
 }
 
 // Obter todos os formandos (Listagem com Turma)
 export async function getFormandos(): Promise<Formando[]> {
-  try {
-    const res = await axios.get(`${API_BASE_URL}/formandos`);
-    return res.data;
-  } catch (error) {
-    console.error("Erro ao obter formandos:", error);
-    throw error;
-  }
+  const res = await axios.get(`${API_BASE_URL}/formandos`);
+  return res.data;
 }
 
 // Obter detalhes de um formando por ID
 export async function getFormandoById(
   idFormando: number | string,
 ): Promise<any> {
-  try {
-    const res = await axios.get(`${API_BASE_URL}/formandos/${idFormando}`);
-    return res.data;
-  } catch (error) {
-    console.error(`Erro ao obter formando ${idFormando}:`, error);
-    throw error;
-  }
+  const res = await axios.get(`${API_BASE_URL}/formandos/${idFormando}`);
+  return res.data;
+}
+
+export async function postNewFormandos(formData: FormData) {
+  const res = await axios.post(`${API_BASE_URL}/formandos/completo`, formData);
+  return res.data;
+}
+
+export async function updateFormando(id: string, data: FormData) {
+  // O Axios configura o Content-Type: multipart/form-data automaticamente ao receber FormData
+
+  const res = await axios.put(`${API_BASE_URL}/formandos/${id}`, data);
+  return res.data;
+}
+
+export async function deleteFormando(idFormando: number) {
+  return axios.delete(`${API_BASE_URL}/Formandos/${idFormando}`);
+}
+
+export async function getTurmas() {
+  const res = await axios.get(`${API_BASE_URL}/turmas`);
+
+  return res.data;
+}
+
+export async function getEscolaridades() {
+  const res = await axios.get(`${API_BASE_URL}/escolaridades`);
+
+  return res.data;
+}
+
+export async function checkEmail(email: string) {
+  const res = await axios.get(
+    `${API_BASE_URL}/utilizadores/details-by-email?email=${email}`,
+  );
+
+  return res.data;
 }
