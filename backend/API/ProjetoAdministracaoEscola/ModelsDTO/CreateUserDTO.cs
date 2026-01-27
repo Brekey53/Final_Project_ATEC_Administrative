007ecc6f1c75b3cef836d98cd3ef9e22cc249db6
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-27 04:40:27 +0000

    feat(CreateUserDTO): new dto to handle new users created by admin/ administrative services

---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/CreateUserDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO
+{
+    public class CreateUserDTO
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Nome { get; set; } = null!;
+        public string Nif { get; set; } = null!;
+        public string Telefone { get; set; } = null!;
+        public DateOnly DataNascimento { get; set; }
+        public string Sexo { get; set; } = null!;
+        public string Morada { get; set; } = null!;
+    }
+}
