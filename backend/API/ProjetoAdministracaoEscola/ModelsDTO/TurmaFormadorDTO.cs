752d7681b6629abbebf3f988da60319815a85417
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-28 05:37:05 +0000

    fix(FormadorDTO): in order to show corresponding data in formador turmas table fixed the variables passed in DTO

--- backend/API/ProjetoAdministracaoEscola/ModelsDTO/TurmaFormadorDTO.cs
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/TurmaFormadorDTO.cs
@@ -4,11 +4,16 @@
     {
         public int IdTurma { get; set; }
         public int IdModulo { get; set; }
-        public string NomeTurma { get; set; }
-        public string NomeModulo { get; set; }
-        public DateOnly DataInicio { get; set; }
-        public DateOnly DataFim { get; set; }
-        public int IdCurso { get; set; }
 
+        public string NomeTurma { get; set; }
+        public string NomeCurso { get; set; }
+        public string NomeModulo { get; set; }
+
+        public double HorasDadas { get; set; }
+        public int HorasTotaisModulo { get; set; }
+
+        public string Estado { get; set; }
     }
+
+
 }
