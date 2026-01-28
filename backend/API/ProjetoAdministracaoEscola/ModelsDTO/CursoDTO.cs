f31aa45268de1f9505a0f745604dac4af8bdf29d
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-28 06:15:13 +0000

    fix(CursoDTO): update Modulos property to be nullable and ensure NomeArea is set correctly

--- backend/API/ProjetoAdministracaoEscola/ModelsDTO/CursoDTO.cs
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/CursoDTO.cs
@@ -5,6 +5,7 @@
         public int IdCurso { get; set; }
         public string Nome { get; set; }
         public int IdArea { get; set; }
-        public List<ModuloDTO> Modulos { get; set; }
+        public string NomeArea { get; set; }
+        public List<ModuloDTO>? Modulos { get; set; }
     }
 }
