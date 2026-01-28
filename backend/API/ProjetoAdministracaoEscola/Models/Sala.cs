1f20b54babb926d65f2602e0d8b1a2afe2f56db4
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-28 05:33:00 +0000

    Add(TipoSala): create TipoSala model with properties and relationship to Sala

--- backend/API/ProjetoAdministracaoEscola/Models/Sala.cs
+++ backend/API/ProjetoAdministracaoEscola/Models/Sala.cs
@@ -7,10 +7,13 @@ public partial class Sala
 {
     public int IdSala { get; set; }
 
+    public int IdTipoSala { get; set; }
+
     public string Descricao { get; set; } = null!;
 
     public int NumMaxAlunos { get; set; }
 
+    public TipoSala IdTipoSalaNavigation { get; set; } = null!;
     public virtual ICollection<DisponibilidadeSala> DisponibilidadeSalas { get; set; } = new List<DisponibilidadeSala>();
 
     public virtual ICollection<Horario> Horarios { get; set; } = new List<Horario>();
