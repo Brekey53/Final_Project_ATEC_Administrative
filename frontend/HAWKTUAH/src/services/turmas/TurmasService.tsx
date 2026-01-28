312e79588c0fb57d3cb481a9ad2a8843df877c65
André Correia <andrecorreis@gmail.com>
2026-01-28 00:55:06 +0000

    Add(TurmasService): methods

--- frontend/HAWKTUAH/src/services/turmas/TurmasService.tsx
+++ frontend/HAWKTUAH/src/services/turmas/TurmasService.tsx
@@ -7,11 +7,36 @@ export interface Turma {
   nomeTurma: string;
   dataInicio: string;
   dataFim: string;
+  nomeCurso: string;
 }
 
 export async function getTurmas() {
   const res = await axios.get(`${API_BASE_URL}/turmas`);
 
+  return res.data;
+}
+
+export async function getTurma(idSala: string) {
+  const res = await axios.get(`${API_BASE_URL}/turmas/${idSala}`);
+
+  return res.data;
+}
+
+export async function deleteTurma(idSala: number) {
+  return axios.delete(`${API_BASE_URL}/turmas/${idSala}`);
+}
+
+export async function postNewTurma(data: any) {
+  return axios.post(`${API_BASE_URL}/turmas`, data);
+}
+
+export async function updateTurma(idSala: string, data: any){
+  const res = await axios.put(`${API_BASE_URL}/turmas/${idSala}`, data);
+  return res.data;
+}
+
+export async function getCursos(){
+  const res = await axios.get(`${API_BASE_URL}/cursos`);
   return res.data;
 }
 
