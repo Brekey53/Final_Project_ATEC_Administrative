c8832628db8c22eba813e3bb413283f0e37f9a7d
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-01-29 04:07:15 +0000

    feat(AvaliacaoDTO, ProfessorDTO, ColegaDTO, MinhaTurma_ModuloDTO): Created for api turmas/minha-turma
    feat(MinhaTurmaDto); main object to join information to send to frontend, grouping teachers, modulos, colegas and information about the turma for that specific user

---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/GetMinhaTurma/ColegaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO.GetMinhaTurma
+{
+    public class ColegaDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+
+}
---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/GetMinhaTurma/ProfessorDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO.GetMinhaTurma
+{
+    public class ProfessorDTO
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
