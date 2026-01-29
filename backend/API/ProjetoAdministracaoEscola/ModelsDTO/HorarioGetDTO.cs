547460b095ccdc22881f002c786b0c122eb152c7
André Correia <andrecorreis@gmail.com>
2026-01-29 01:30:19 +0000

    Create(HorarioGetDTO)

---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/HorarioGetDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO
+{
+    public class HorarioGetDTO
+    {
+        public int IdHorario { get; set; }
+
+        public string NomeTurma { get; set; } = null!;
+
+        public string NomeCurso { get; set; } = null!;
+
+        public string NomeModulo { get; set; } = null!;
+
+        public string NomeFormador { get; set; } = null!;
+
+        public string NomeSala { get; set; } = null!;
+
+        public DateOnly Data { get; set; }
+
+        public TimeOnly HoraInicio { get; set; }
+
+        public TimeOnly HoraFim { get; set; }
+    }
+}
