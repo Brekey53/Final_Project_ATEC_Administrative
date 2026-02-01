b078018f57a29d86c1170846ff3cc9b257f3cc5f
André Correia <andrecorreis@gmail.com>
2026-02-01 00:42:45 +0000

    Add(TurmaFormadorModuloCurso): new DTO

---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/TurmaFormadorModuloCursoDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO
+{
+    public class TurmaFormadorModuloCursoDTO
+    {
+        public int IdTurma { get; set; }
+        public int IdModulo { get; set; }
+        public int IdCursoModulo { get; set; }
+
+        public string NomeTurma { get; set; }
+        public string NomeCurso { get; set; }
+        public string NomeModulo { get; set; }
+
+        public double HorasDadas { get; set; }
+        public int HorasTotaisModulo { get; set; }
+        public string Estado { get; set; }
+    }
+}
