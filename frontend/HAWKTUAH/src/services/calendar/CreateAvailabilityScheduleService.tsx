e214c78fff908868400c397d4174ded62a029233
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-02-04 02:50:12 +0000

    feat(CreateAvailabilityScheduleService); added service to get, post and delete for professor

---
+++ frontend/HAWKTUAH/src/services/calendar/CreateAvailabilityScheduleService.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { API_BASE_URL } from "../../config.constants";
+
+export interface AvailabilityEvent {
+  id?: number;
+  idFormador: number;
+  data: string;
+  horaInicio: string;
+  horaFim: string;
+}
+
+export interface ScheduleEvent {
+  data: string;
+  horaInicio: string;
+  horaFim: string;
+}
+
+export async function getEventosCalendario() {
+  const response = await axios.get(API_BASE_URL + "/disponibilidadeformador");
+  return response.data;
+}
+
+export async function postEventosCalendario(eventData: ScheduleEvent) {
+  const response = await axios.post(API_BASE_URL + "/disponibilidadeformador", {
+    ...eventData,
+  });
+  return response.data;
+}
+
+export async function deleteEventosCalendario(id: number) {
+  const response = await axios.delete(
+    API_BASE_URL + `/disponibilidadeformador/${id}`,
+  );
+  return response.data;
+}
