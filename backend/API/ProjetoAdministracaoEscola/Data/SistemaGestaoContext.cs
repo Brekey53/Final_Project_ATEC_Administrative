f29b9bd3046c8601fd089d943cf2285fccdbab1b
André Correia <andrecorreis@gmail.com>
2026-02-04 01:03:16 +0000

    Add(SistemaGestaoContext): add QueryFilter "global"

--- backend/API/ProjetoAdministracaoEscola/Data/SistemaGestaoContext.cs
+++ backend/API/ProjetoAdministracaoEscola/Data/SistemaGestaoContext.cs
@@ -600,6 +600,13 @@ public partial class SistemaGestaoContext : DbContext
 
         });
 
+        modelBuilder.Entity<Utilizador>().HasQueryFilter(e => e.Ativo);
+        modelBuilder.Entity<Formador>().HasQueryFilter(e => e.Ativo);
+        modelBuilder.Entity<Formando>().HasQueryFilter(e => e.Ativo);
+        modelBuilder.Entity<Curso>().HasQueryFilter(e => e.Ativo);
+        modelBuilder.Entity<Modulo>().HasQueryFilter(e => e.Ativo);
+        modelBuilder.Entity<Turma>().HasQueryFilter(e => e.Ativo);
+
         OnModelCreatingPartial(modelBuilder);
     }
 
