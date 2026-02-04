4ef6b79fbcd056d3d6fe72f9343939adad769cbd
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-02-04 02:23:35 +0000

    feat(DTO): added DTOS for requests and responses for formador availability

---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/DisponibilidadeFormador/DisponibilidadeFormadorDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO.DisponibilidadeFormador
+{
+    public class DisponibilidadeFormadorDTO
+    {
+        public int Id { get; set; }
+
+        public int IdFormador {get; set;}
+
+        public DateOnly Data { get; set;}
+
+        public TimeOnly HoraInicio { get; set;}
+
+        public TimeOnly HoraFim { get; set; }
+
+    }
+}
---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/DisponibilidadeFormador/Request/AdicionarDisponibilidadeFormadorDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO.DisponibilidadeFormador.Request
+{
+    public class AdicionarDisponibilidadeFormadorDTO
+    {
+        public string Data { get; set; }
+
+        public string HoraInicio { get; set; }
+
+        public string HoraFim { get; set; }
+    }
+}
