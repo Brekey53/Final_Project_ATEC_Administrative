d869d2a6b392dd21538ccfbdad401909b2fdbecd
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-02-05 05:46:53 +0000

    feat(calendar): added buttons for month, week, day. and added mobile responsiveness

--- frontend/HAWKTUAH/src/components/CalendarSchedule.tsx
+++ frontend/HAWKTUAH/src/components/CalendarSchedule.tsx
@@ -1,12 +1,16 @@
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import ptLocale from "@fullcalendar/core/locales/pt";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 export default function CalendarSchedule({ events }: { events: any[] }) {
+  const isMobile = window.innerWidth < 768;
+
   return (
     <FullCalendar
-      plugins={[timeGridPlugin]}
-      initialView="timeGridWeek"
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      initialView={isMobile ? "timeGridDay" : "timeGridWeek"}
       locale={ptLocale}
       events={events}
       allDaySlot={false}
@@ -14,6 +18,19 @@ export default function CalendarSchedule({ events }: { events: any[] }) {
       slotMaxTime="23:00:00"
       height="auto"
       nowIndicator
+      headerToolbar={
+        isMobile
+          ? {
+              left: "prev,next",
+              center: "title",
+              right: "",
+            }
+          : {
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }
+      }
     />
   );
 }
