145714ee3f50c6510b28567ddced34131f8d4c86
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-02-05 05:45:10 +0000

    feat(dto): added dto to respond data for top 10 formadores

---
+++ backend/API/ProjetoAdministracaoEscola/ModelsDTO/Formador/TopFormadoresDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoAdministracaoEscola.ModelsDTO.Formador
+{
+    public class TopFormadoresDTO
+    {
+        public string Nome { get; set; }
+
+        public double Horas { get; set;}
+    }
+}
