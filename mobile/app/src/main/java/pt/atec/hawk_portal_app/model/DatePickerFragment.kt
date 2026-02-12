e50f7558499d9561271cd2ae2bbf75a41b586ed3
André Correia <andrecorreis@gmail.com>
2026-02-12 00:08:52 +0000

    DatePicker class for calendar

---
+++ mobile/app/src/main/java/pt/atec/hawk_portal_app/model/DatePickerFragment.kt
@@ -0,0 +1,31 @@
+package pt.atec.hawk_portal_app.model
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.icu.util.Calendar
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+
+class DatePickerFragment(
+    val onDateSelected: (year: Int, month: Int, day: Int) -> Unit = { _, _, _ -> }
+) : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the current date as the default date in the picker.
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        // Create a new instance of DatePickerDialog and return it.
+        return DatePickerDialog(requireContext(), this, year, month, day)
+
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+        // Do something with the date the user picks.
+        onDateSelected(year, month, day)
+
+    }
+}
