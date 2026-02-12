db00c93ff6c95ef9520f56bae4a8e9e0f6999d04
Leonor Joaquim <leonorjoaquim98@gmail.com>
2026-02-12 07:01:43 +0000

    created new file with right name

---
+++ frontend/HAWKTUAH/src/services/users/CreateAccountNewUser.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { API_BASE_URL } from "../../config.constants";
+
+export interface RegisterData {
+  Email: string;
+  Password: string;
+  Nome: string;
+  Nif: string;
+  DataNascimento: string; // Formato YYYY-MM-DD
+}
+
+/**
+ * Função de Registo de Utilizadores
+ * Envia os dados para a tabela centralizada de Utilizadores
+ */
+export async function CreateAccountNewUser(userData: RegisterData) {
+  try {
+    const res = await axios.post(
+      `${API_BASE_URL}/auth/register`,
+      userData,
+    );
+
+    return res.data;
+  } catch (error: any) {
+    console.error("Erro no Registo:", error.response?.data || error.message);
+
+    throw error;
+  }
+}
